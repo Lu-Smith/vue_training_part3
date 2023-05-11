5b2d47d45ecb4e93433407bc3d107720e4ae1766
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-11 14:35:47 +0100

    Playing with refs

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <h1>{{title}}</h1>
+  <input type="text" ref="name" />
+  <button @click="handleClick">click me</button>
+
+
 </template>
 
 <script>
@@ -8,7 +12,13 @@ export default {
   name: 'App',
   data() {
     return {
-      title: 'Training Vue App'
+      title: 'Training Vue App 🙂'
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
     }
   }
 }
@@ -22,5 +32,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
